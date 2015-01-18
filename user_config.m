0eddc27cbc37d5de81ed7f78fc20275be5c895fe
Jakob Unterwurzacher <jakobunt@gmail.com>
2015-01-18 17:54:18 +0100

    Rename coilcontrol_settings.m to user_config.m
    This makes clear that this a config file users should edit.

--- user_config.m
+++ user_config.m
@@ -1,14 +1,17 @@
-function [ s ] = coilcontrol_settings( )
+function [ s ] = user_config( )
 %_coilcontrol_settings Settings for coilcontrol that might need adjustment when
 % moving to a different computer
 
-% What COM port number are the power supplies connected to
-s.psucom.x=4;
-s.psucom.y=5;
+s.psucom.x=4; % COM port number
+s.psuout.x=1; % Output channel (for dual output PSUs)
+
+s.psucom.y=4;
+s.psuout.y=2;
+
 s.psucom.z=6;
+s.psuout.z=1;
 
 % What COM port number is the Bartington Mag03DAM connected to
 s.mag03com=2;
 
 end
-
